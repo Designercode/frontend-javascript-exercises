a884d538d187ac13851d5ce403908668d574172a
Designercode <april.cummings1@gmail.com>
2014-11-13 16:30:52 +0000

    Finished Booleans

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,83 @@
-module.exports.equalStrings = function(stringOne, stringTwo) {
-
+//Exercise 1
+var equalStrings = function(stringOne, stringTwo) 
+{
+  var myBool = (stringOne == stringTwo);
+  return myBool;
 };
 
-module.exports.notEqual = function(one, two) {
+equalStrings("fun", "fun");
+equalStrings("bloc", "boring");
 
+
+//Excercise 2
+var notEqual = function(one, two) 
+{
+  var myBool = (one !== two);
+  return myBool;
 };
 
-module.exports.inBetween = function(lower, middle, upper) {
+notEqual(1,'1');
+notEqual(1,1);
 
+//Excercise 3
+var inBetween = function(lower, middle, upper) 
+{
+  var myBool = ((lower < middle) && (middle < upper));
+  return myBool;
 };
 
-module.exports.outsideRanges = function(number) {
+inBetween(1,3,5);
+inBetween(3,1,5);
 
+//Excercise 4
+var outsideRanges = function(number) 
+{
+  var between = (!(number >=10) && !(number <=20));
+  var exact = (!(number <42) && !(number >42));
+  var not = (!(number <=75));
+  var notBetween = (!(number >1) && !(number <6));
+  if(between == true)
+  {
+    if (exact == true)
+    {
+      if(not == true)
+      {
+        if(notBetween == true)
+        {
+          return true;
+        }
+      }
+    }
+  }
+return false;
 };
 
-module.exports.nameAndPrice = function(name, price) {
+outsideRanges(42);
+outsideRanges(75);
 
+//Excercise 5
+var nameAndPrice = function(name, price) 
+{
+  if (((name == 'NYTimes') || (name = 'LATimes')) && (price >= 1));
+    {
+      return true;
+    }
+ return false;
 };
+
+nameAndPrice('NYTimes', 1);
+nameAndPrice('LATimes', 0);
+nameAndPrice('NYTimes', 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
