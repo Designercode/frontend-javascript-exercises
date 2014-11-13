7c0592aa8751c3d59283a18d8fefbc57ea37fc29
Designercode <april.cummings1@gmail.com>
2014-11-13 17:27:20 +0000

    String Exercises

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,60 @@
-module.exports.formLetter = function(firstName, senderName, message) {
-
+// Excercise 1: Combine
+var formLetter = function(firstName, senderName, message)
+{
+  var greeting = "Hello";
+  firstName = ("\n\n" + firstName + ",");
+  senderName = ("\n" + senderName); 
+  message = ("\n\n" + message + "\n\nSincerely,");
+  greeting = (greeting + firstName + message + senderName);
+  return greeting;
 };
 
-module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
+formLetter("James", "Richard", "Things are well.");
 
+
+//Excercise 2: Slice & Combine
+var sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) 
+{
+  var stringA = bigString.substring(startA, endA);
+  var stringB = bigString.substring(startB,endB);
+  var stringC = stringA + stringB;
+  return stringC;
 };
 
-module.exports.findFirstMatch = function(text, searchString) {
+sliceItAndCombineIt("This is a test",7,2,8,5)
 
+
+//Excercise 3: First Match
+var findFirstMatch = function(text, searchString) 
+{
+var found = text.indexOf(searchString); 
+return found;
 };
 
-module.exports.findLastMatch = function(text, searchString) {
+findFirstMatch("Roses are red!", "re");
 
+
+//Excercise 4: Last Match
+var findLastMatch = function(text, searchString) 
+{
+  var found = text.lastIndexOf(searchString);
+  return found;
 };
 
-module.exports.substringBetweenMatches = function(text, searchString) {
+findLastMatch("Roses are red!", "re");
 
+
+//Exercise 5: Substring
+var substringBetweenMatches = function(text, searchString) 
+{
+  var first = text.indexOf(searchString);
+  var last = text.lastIndexOf(searchString);
+  var start = (first + 1);
+  var end = (last - 1);
+  return text.substring(start,end);
 };
+
+substringBetweenMatches("Roses are red, apples are really red.", "red");
+
+
+
