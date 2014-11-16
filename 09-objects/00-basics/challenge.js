d8f38bd49b61c990c4539ae48be94f05ea27bfa1
Designercode <april.cummings1@gmail.com>
2014-11-16 12:31:00 +0000

    Finsihed Objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,78 @@
-module.exports.createCourse = undefined;
+//module.exports.createCourse = undefined;
 
-module.exports.addProperty = undefined;
+var createCourse = function(x,y,z)
+{
+  var course = new Object;
+  course = 
+  {
+    courseTitle: x,
+    courseDuration: y,
+    courseStudents: z,
+  };
+    console.log(course);
+    return course;
+};
 
-module.exports.formLetter = undefined;
+createCourse('Bloc Front-End Engineering', '4 weeks',['Joe','Tim','Rob'])
 
-module.exports.canIGet = undefined;
+//module.exports.addProperty = undefined;
+
+var addProperty = function(object,newProp,newValue)
+{
+  //var object = new Object; // ("object" is created but its properties remain unassigned. - is first assigned with input)
+    if (object.hasOwnProperty('firstName')) 
+      {
+        console.log(object);
+      }
+    else
+      {
+        object[newProp] = newValue; // REMEMBER: object[x] = content (property name) of variable x |BUT| object['x'] or object.x = x as property name
+        console.log(object);
+      }
+  return object;
+};
+
+addProperty({}, 'firstName', 'Jim'); // returns {firstName : 'Jim'} (adds property and name) -> REMEMBER: Object is already created, but is empty!
+addProperty({firstName: 'Rob'}, 'firstName', 'Jim'); // returns {firstName : 'Rob'} (doesn't add property, and doesn't overwrite name)
+
+
+//module.exports.formLetter = undefined;
+
+var formLetter = function(letter)
+{
+  var newLetter = "Hello " + letter.recipient +", \n\n" + letter.msg + "\n\nSincerely, \n"+ letter.sender;
+  console.log(newLetter);
+
+};
+
+formLetter({recipient: "James", sender: "Richard", msg: "Things are well."});
+
+//module.exports.canIGet = undefined;
+
+var canIGet = function(item, money)
+{
+var priceList =
+  {
+    'MacBook Air': 999,
+    'MacBook Pro': 1299,
+    'Mac Pro': 2499,
+    'Apple Sticker': 1,
+  };
+
+  if (priceList.hasOwnProperty(item))
+  {
+    if (money >= priceList[item])
+    {
+      console.log("true");
+      return true;
+    }
+  }
+  console.log("false");
+  return false;
+};
+
+canIGet('MacBook Air', 100);
+canIGet('MacBook Air', 1000);
+
+
+
