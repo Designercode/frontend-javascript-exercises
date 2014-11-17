d6fcbe63ebc7cd2cf0934eac32aa0f8eb7d27ef7
Designercode <april.cummings1@gmail.com>
2014-11-17 06:41:21 +0000

    Functions Update

--- 06-functions/00-basics/challenge.js
+++ 06-functions/00-basics/challenge.js
@@ -8,9 +8,18 @@ var hello = function()
 
 //module.exports.multiply = undefined;
 
+
 var multiply = function(x,y)
 {
-  return x*y;
+  var isNum = isNaN(x);
+  var isAlsoNum = isNaN(y);
+    if ((isNum == false) && (isAlsoNum == false));
+    {
+      return (x*y);
+    }
+    return console.log("Sorry, you did not enter number values.");
 };
 
 multiply(10,10); 
+multiply(a,b);
+
